55f6c035fd701c2f51b7ed2c05edac3ea9767c73
TheeBrainiacs <145912020+Kronbii@users.noreply.github.com>
2024-10-13 23:49:17 +0300

    anh homsi betrajak sa3edni

--- src/Screens/About.jsx
+++ src/Screens/About.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 function About() {
+  const { t, i18n } = useTranslation();
   return (
     <div>
-      <h1>About</h1>
+      <h1>{t("about.title.aboutUs")}</h1>
+      <p className="about-description">
+        {t("about.description")}
+      </p>
+
+      <h1>{t("about.title.mission")}</h1>
+      <p className="about-mission">
+        {t("about.mission")}
+      </p>
+
+      <h1>{t("about.title.vision")}</h1>
+      <p className="about-vision">
+        {t("about.vision")}
+      </p>
+
+       <h1>{t("about.title.joinUs")}</h1>
+      <p className="about-join">
+        {t("about.joinUs")}
+      </p>
     </div>
   );
 }
