a20932c26c95fc63ec39e069d85ed44c717c47cc
Mohamad Homsi <homsimohamad4@gmail.com>
2026-03-14 16:39:03 +0200

    chore: move Firebase measurement ID to env var

--- src/firebase.ts
+++ src/firebase.ts
@@ -18,7 +18,7 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: 'G-NGHT9F7LLJ',
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
